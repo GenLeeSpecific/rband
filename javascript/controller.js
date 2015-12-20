542054267aef18bd36c9f80d9733e05ca8dc0158
Mehran Hatami <fixmyjs@gmail.com>
2015-12-20 10:34:38 +0000

    latest chnages from Will

--- javascript/controller.js
+++ javascript/controller.js
@@ -9,7 +9,6 @@ angular.module('routingControllers', [])
   //put your code here
  })
  .controller('GigsController', function ($scope) {
-
   $scope.toggle = function (type) {
    $('.main-container .weddings-corporate-container .' + type + '-text').toggle(1000);
   };
@@ -18,11 +17,11 @@ angular.module('routingControllers', [])
   //put your code here
  })
  .controller('BiosController', function ($scope) {
-  $scope.toggle = function (type) {
-   $('.main-container .weddings-corporate-container .' + type + '-text').toggle(1000);
-  };
  })
  .controller('UpcomingController', function ($scope) {
+  //put your code here
+ })
+ .controller('WeddingsAndCorporateController', function ($scope) {
   //put your code here
  })
  .controller('ContactAndBookingsController', function ($scope) {
